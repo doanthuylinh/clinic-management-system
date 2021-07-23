feb78f4063cbb25938d3962ca28546a5349b2b77
LinhDT <thuylinhdoan1999@gmail.com>
2021-07-23 21:45:16 +0700

    fix(CORS): function getMedicineById

--- src/main/java/hellocs/clinic_management_system/controller/MedicineController.java
+++ src/main/java/hellocs/clinic_management_system/controller/MedicineController.java
@@ -11,6 +11,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -77,6 +78,7 @@ public class MedicineController {
      * @param medicineId
      * @return
      */
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/medicine/{medicineId}", method = RequestMethod.GET)
     public ResponseEntity<ResultBean> getMedicineById(@PathVariable Integer medicineId) {
 
@@ -141,7 +143,7 @@ public class MedicineController {
         LOGGER.info("----------updateMedicine END----------");
         return new ResponseEntity<ResultBean>(resultBean, ResponseUtils.getResponseStatus(resultBean));
     }
-    
+
     /**
      * deleteMedicine
      * @author: LinhDT
@@ -149,20 +151,20 @@ public class MedicineController {
      * @return
      */
     @RequestMapping(value = "/medicine", method = RequestMethod.DELETE)
-    public ResponseEntity<ResultBean> deleteMedicine(@RequestBody String data){
+    public ResponseEntity<ResultBean> deleteMedicine(@RequestBody String data) {
         LOGGER.info("----------deleteMedicine START----------");
-        
+
         ResultBean resultBean = null;
-        
+
         try {
             resultBean = medicineService.deleteMedicine(data);
-        }catch(ApiValidateException e) {
-            resultBean = new ResultBean(e.getCode(),e.getMessage());
-        }catch(Exception e) {
+        } catch (ApiValidateException e) {
+            resultBean = new ResultBean(e.getCode(), e.getMessage());
+        } catch (Exception e) {
             e.printStackTrace();
             resultBean = new ResultBean("500", "Internal server error");
         }
-        
+
         LOGGER.info("----------deleteMedicine END----------");
         return new ResponseEntity<ResultBean>(resultBean, ResponseUtils.getResponseStatus(resultBean));
     }
